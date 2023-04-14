de9c79231a955dbc385a3a7201d8ead8e1d36373
Dnyaneshwarikad <dnyaneshwarimungase@gmail.com>
2023-04-14 23:38:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assignment6';
+export class AppComponent
+
+{
+     public mycolor ="blue";
+     public color ="changecolor";
+
 }
+
